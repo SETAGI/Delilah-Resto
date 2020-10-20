d0593082df033764abe075349530cdb0ce2e4564
SETAGI <tamayo0711@hotmail.com>
2020-10-19 21:05:16 -0500

    Add config db

--- database/db.js
+++ database/db.js
@@ -1,9 +1,11 @@
 const Sequelize = require('sequelize');
 // const sequelize = new Sequelize(`mysql://root:@localhost:3306/delilahdb`);
 
-const sequelize = new Sequelize('delilahdb', 'root', '', {
-	host: 'localhost',
+const sequelize = new Sequelize('heroku_a063db398612875', 'b7e1f18868983b', '6f0588fb', {
+	host: 'us-cdbr-east-02.cleardb.com',
 	dialect: 'mysql',
 });
 
-module.exports = { sequelize };
+//b7e1f18868983b:6f0588fb@us-cdbr-east-02.cleardb.com/heroku_a063db398612875?reconnect=true'
+
+mysql: module.exports = { sequelize };
